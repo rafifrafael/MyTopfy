090e390186a3b612d729009a370f9b65862d659a
Rafif <rafifmurtadho@gmail.com>
2023-07-29 01:21:59 +0800

    v1.0.0

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>MyTopfy</title>
+</head>
+
+<body>
+    <main>
+        <div class="container py-4">
+          <header class="pb-3 mb-4 border-bottom">
+            <nav class="navbar bg-body-tertiary">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="https://mytopfy.vercel.app/">
+                  MyTopfy
+                </a>
+              </div>
+            </nav>
+          </header>
+      
+          <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+              <h1 class="display-5 fw-bold">Read Me</h1>
+              <p class="col-md-8 fs-4">Because the <strong>Spotify API</strong> requires approval to be used with your respective accounts, for now it can only be used with the account that I have provided below. 
+                Hopefully Spotify can approve my application so you can try using your own account. </p>
+            </div>
+          </div>
+      
+          <div class="row align-items-md-stretch">
+            <div class="col-md-6">
+              <div class="h-100 p-5 text-bg-dark rounded-3">
+                <h2>Testing Account</h2>
+                <p>Email : 	vavim56858@sportrid.com</p>
+                <p>Password : mytopfytesting</p>
+                <a href="/" target="_blank" class="btn btn-outline-light" type="button">Try now!</a>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="h-100 p-5 bg-light border rounded-3">
+                <h2>Using Your Account</h2>
+                <p>If you still want to use your account, you can send me the <strong>email</strong> you use to log into Spotify.</p>
+                
+                <a href="mailto:rafifmurtadho@gmail.com" class="btn btn-outline-secondary" type="button">Contact Me</a>
+              </div>
+            </div>
+          </div>
+      
+          <footer class="pt-3 mt-4 text-muted border-top">
+            © 2023 Rafif Murtadho
+          </footer>
+        </div>
+    </main>
+
+    <script src="https://kit.fontawesome.com/237c69a9d4.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+
+</body>
+</html>
