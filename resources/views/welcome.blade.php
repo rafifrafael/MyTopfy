08338f11ca106922ca8fe0f51e4c7b70406a2866
Rafif <rafifmurtadho@gmail.com>
2023-07-28 19:40:57 +0800

    v1.0.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    @vite
+    @vite('resources/css/app.css')
+
 
     <title>MyTopfy</title>
 
