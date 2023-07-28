2a379120cf1720d625a5f2fcf99aae0aa2665e81
Rafif <rafifmurtadho@gmail.com>
2023-07-28 20:42:33 +0800

    v1.0.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @vite('resources/css/app.css')
