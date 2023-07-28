a9b2d5546eff23271f40bcc66f43bc5eb56811dd
Rafif <rafifmurtadho@gmail.com>
2023-07-28 21:49:05 +0800

    v1.0.0

--- config/app.php
+++ config/app.php
@@ -55,9 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-
-    'asset_url' => env('ASSET_URL'),
+    'url' => env('APP_URL', 'https://mytopfy.vercel.app'),
 
     /*
     |--------------------------------------------------------------------------
